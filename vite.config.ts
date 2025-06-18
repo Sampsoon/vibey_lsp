256f2631828dfdfd5cef627a95f6fc6a4313ab86
sampsoon <28665112+Sampsoon@users.noreply.github.com>
2025-06-17 21:55:56 -0400

    Migrate to vite to use @crxjs/vite-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve, join, basename, extname } from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { readdirSync, statSync } from 'fs';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-function listFilesInDirectory(dir: string): string[] {
-  const absoluteDir = resolve(__dirname, dir);
-  return readdirSync(absoluteDir)
-    .map(name => join(absoluteDir, name))
-    .filter(path => statSync(path).isFile());
-}
-
-function entryMap(paths: string[]): Record<string, string> {
-  return Object.fromEntries(
-    paths.map(path => [basename(path, extname(path)), path])
-  );
-}
-
-const serviceWorkerInputMap = entryMap(listFilesInDirectory('src/serviceWorkers'));
-const contentScriptInputMap = entryMap(listFilesInDirectory('src/contentScripts'));
-
-const serviceWorkerInputNames = Object.keys(serviceWorkerInputMap);
-const contentScriptInputNames = Object.keys(contentScriptInputMap);
+import { crx, ManifestV3Export } from '@crxjs/vite-plugin';
+import manifest from './public/manifest.json';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    crx({
+      manifest: manifest as ManifestV3Export,
+      browser: 'chrome',
+    }),
+  ],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        popup: resolve(__dirname, 'index.html'),
-        ...serviceWorkerInputMap,
-        ...contentScriptInputMap,
-      },
-      output: {
-        entryFileNames: chunkInfo => {
-          if (serviceWorkerInputNames.includes(chunkInfo.name)) {
-            return 'serviceWorkers/[name].js';
-          }
-          if (contentScriptInputNames.includes(chunkInfo.name)) {
-            return 'contentScripts/[name].js';
-          }
-          return 'assets/[name]-[hash].js';
-        },
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash][extname]'
-      }
-    },
-    copyPublicDir: true
-  }
+    copyPublicDir: true,
+  },
 });
