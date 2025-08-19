602c72cd1d15cc8b9fc6696e7a01c4fd963fa8e4
sampsoon <28665112+Sampsoon@users.noreply.github.com>
2025-08-18 21:57:49 -0400

    Clean up exports

--- src/hoverHints/index.ts
+++ src/hoverHints/index.ts
@@ -1,12 +1,3 @@
-export type {
-  DocString,
-  HoverHint,
-  HoverHintList,
-  HoverHintState,
-  ParamDocString,
-  ReturnDocString,
-  isReturnDocString,
-  isParamDocString,
-} from './types';
+export type { HoverHint, HoverHintList, HoverHintState } from './types';
 export { hoverHintListSchema, hoverHintSchema } from './types';
 export { attachHoverHint, setupHoverHintTriggers, setupHoverHintState } from './hoverHintAttachment';
